09c48b2dacbe2865c38105c92fb066d16f3675e0
Robin <dozesoft@gmail.com>
2026-08-08 12:04:48 +0900

    Delay gimmick visual destruction to sync with tiles

--- scripts/elements/base_element.gd
+++ scripts/elements/base_element.gd
@@ -73,6 +73,8 @@ func take_damage(amount: int = 1) -> void:
 
 func destroy() -> void:
 	element_destroyed.emit(self)
+	if is_inside_tree() and get_tree() != null:
+		await get_tree().create_timer(0.2).timeout
 	_spawn_debris()
 	queue_free()
 
